2ff24add74b615ffb4869caffb4f849c2f67904c
Dubravko <woose@web.de>
2018-05-03 11:51:10 +0200

    secret

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import SearchBar from "./components/search_bar";
+import Secret from "./secret/secret";
 
-const API_KEY = "AIzaSyAGZ1JSi6aJ8BN4KyVjqHhQH6bAas0tVaA";
+const secret = Secret;
 
 const App = () => {
     return ( < div >
---
+++ src/secret/secret.js
@@ -0,0 +1,3 @@
+const API_KEY = "AIzaSyAGZ1JSi6aJ8BN4KyVjqHhQH6bAas0tVaA";
+
+export default API_KEY;
