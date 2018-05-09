a620e4cb950ecdec537940508daf679137ec9521
Dubravko <woose@web.de>
2018-05-09 14:11:24 +0200

    function als property in die video_list Komponente einfügen

--- src/index.js
+++ src/index.js
@@ -14,18 +14,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({ key: secret, term: "react redux" }, videos => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={video =>
+            this.setState({
+              selectedVideo: video
+            })
+          }
+          videos={this.state.videos}
+        />
       </div>
     );
   }
